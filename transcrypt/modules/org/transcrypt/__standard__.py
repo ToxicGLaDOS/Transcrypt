a2fc48fd05631b26294de4d71c2d0dd6747280b8
David Ross <daboross@daboross.net>
2017-07-27 00:18:30 -0700

    Find stacktraces for errors where an original error with a stacktrace was not provided.

--- transcrypt/modules/org/transcrypt/__standard__.py
+++ transcrypt/modules/org/transcrypt/__standard__.py
@@ -14,9 +14,11 @@ class Exception:
     __pragma__ ('kwargs')
     def __init__ (self, *args, **kwargs):
         self.__args__ = args
-        try:
+        if kwargs.error != None:
             self.stack = kwargs.error.stack # Integrate with JavaScript Error object
-        except:
+        elif Error:
+                self.stack = (__new__(Error())).stack
+        else:
             self.stack = 'No stack trace available'
     __pragma__ ('nokwargs')
         
